798049eca99f0b0c96cbf4fa4f6069ca3bc3838f
Nader Chargui <charguinadar@gmail.comm>
2025-11-11 04:17:06 +0100

    add some function about the String

--- main.ts
+++ main.ts
@@ -1,15 +1,31 @@
 import clsStringLibrary from "./src/StringLibrary";
 
 function main(): void {
-    const obj1 = new clsStringLibrary();
-    const obj2 = new clsStringLibrary("Hello from clsString library");
+  const obj1 = new clsStringLibrary();
 
+  obj1.setValue("Hello With Typescript from clsString library");
 
-    console.log("Object 1 value:", `"${obj1.getValue()}"`, "Word Count:", obj1.CountWords());
-    console.log("Object 2 value:", `"${obj2.getValue()}"`, "Word Count:", clsStringLibrary.CountWords(obj2.getValue()));
-   
+  console.log(
+    "String 1 of obj1 value:",
+    `${obj1.getValue()}`,
+    "\t\tWord Count:",
+    obj1.CountWords()
+  );
+  console.log(
+    "String 2 of obj2 value:",
+    `Nader Chargui is learning TypeScript`,
+    "\tWord Count:",
+    clsStringLibrary.CountWords("Nader Chargui is learning TypeScript")
+  );
+
+  console.log(
+    "String 2 of obj2 vowels are :",
+    `Nader Chargui is learning TypeScript`,
+    "\tCount vowels:",
+    obj1.CountVowels()
+  );
+
+  console.log("\nupper words are :", clsStringLibrary.UpperEachWord("hello nader how are you"));
 }
 
 main();
-
-
--- src/StringLibrary.ts
+++ src/StringLibrary.ts
@@ -1,48 +1,97 @@
+//A, E, I, O, U
+// let letter: string = 'A';
 export default class clsStringLibrary {
-    private _value: string;
+  private _value: string;
 
-    constructor(value?: string) {
-        this._value = value || "";
+  constructor(value?: string) {
+    this._value = value || "";
+  }
+
+  public getValue(): string {
+    return this._value;
+  }
+
+  public setValue(value: string): void {
+    this._value = value;
+  }
+
+  // public CountWords(): number;
+  // public CountWords(str: string): number;
+
+  private static IsVOWELetter(char: string): boolean {
+    if (char.length !== 1 || char === undefined) return false;
+    if (
+      char === "A" ||
+      char === "E" ||
+      char === "I" ||
+      char === "O" ||
+      char === "U" ||
+      char === "a" ||
+      char === "e" ||
+      char === "i" ||
+      char === "o" ||
+      char === "u"
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  public static CountWords(str: string): number {
+    if (!str) return 0;
+
+    let count = 0;
+    let pos = 0;
+    let sword = "";
+    let separator = " ";
+
+    while ((pos = str.indexOf(separator)) !== -1) {
+      sword = str.substring(0, pos);
+      if (sword !== "") count++;
+      str = str.substring(pos + separator.length);
+    }
+    if (str !== "") count++;
+
+    return count;
+  }
+
+  public CountWords(): number {
+    const strNew = this.getValue();
+
+    return clsStringLibrary.CountWords(this._value);
+  }
+
+  public static CountVowels(str: string): number {
+    let count: number = 0;
+    for (let i = 0; i < str.length; i++) {
+      if (this.IsVOWELetter(str[i])) {
+        count++;
+      }
     }
 
-    public getValue(): string {
-        return this._value;
+    return count;
+  }
+
+  public CountVowels(): number {
+    return clsStringLibrary.CountVowels(this._value);
+  }
+
+  public static UpperEachWord(str: string): string {
+    let isExistSpace: boolean = true;
+    let result = "";
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] !== " " && isExistSpace) {
+        result += str[i].toUpperCase();
+      } else {
+        result += str[i];
+      }
+
+      isExistSpace = str[i] === " ";
     }
+    return result;
+  }
 
-    public setValue(value: string): void {
-        this._value = value;
-    }
-
-    // public CountWords(): number;
-    // public CountWords(str: string): number;
-
-    public static CountWords(str: string): number {
-       // str = str === undefined ? this._value : str;
-        if (!str) return 0;
-
-        let count = 0;
-        let pos = 0;
-        let sword = "";
-        let separator = " ";
-
-        while ((pos = str.indexOf(separator)) !== -1) {
-            sword = str.substring(0, pos);
-            if (sword !== "") count++;
-            str = str.substring(pos + separator.length);
-        }
-        if (str !== "") count++;
-        console.log("count is ", count);
-        return count;
-    }
-
-    public  CountWords(): number {
-         const strNew = this.getValue();
-        // const tempObj = new clsLibraryString(strNew);
-        // console.log("inside function STATIC, object:", obj, "value:", strNew);
-     //   console.log("type of obj:", typeof obj,"type of method:", typeof obj.CountWords);
-      //  obj._value = "just to try every thing ok nader"
-     
-     //   return this.CountWords(this._value);
-        return clsStringLibrary.CountWords(this._value); // ✅ call static method
-    }
+  public UpperEachWord(): string {
+    return clsStringLibrary.UpperEachWord(this._value);
+  }
 }
